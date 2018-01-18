b081a53fb12db2889dc75cab3375a6a711bd719c
dotJS <justdotJS@icloud.com>
2018-01-18 11:41:20 +0800

    Update run.py

--- run.py
+++ run.py
@@ -32,6 +32,7 @@ from QuadFile.output import print_log, time_to_string
 from QuadFile import application
 
 app = Flask(__name__)
+app.debug = True
 app.secret_key = constants.SECRET_KEY
 
 oauth = OAuth(app)
